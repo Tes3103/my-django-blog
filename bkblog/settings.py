4db2706641a58178f95bcf93c0beaa89ce7d58b3
Tesfaye Wolde <tesfekadu21@gmail.com>
2024-04-17 20:39:21 +0000

    deployment setting number 2

--- bkblog/settings.py
+++ bkblog/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5dx8%pz#$lbu)=#&r_1co+^yz@oefwp#b%u9v7e#ej3jd)#y=f'
+# SECRET_KEY = 'django-insecure-5dx8%pz#$lbu)=#&r_1co+^yz@oefwp#b%u9v7e#ej3jd)#y=f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
